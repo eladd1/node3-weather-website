c6db3c4211e738a7aa6a5b5727d11fcbee010ccb
Elad De-Roos <eladd@exlibrisgroup.com>
2019-10-27 15:10:17 +0200

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+console.log('Client side javascript file is loaded!')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    console.log(location)
+    fetch('http://localhost:3000/weather?address=' + location).then((response, error) => {
+    
+        response.json().then((data) => {
+            if(data.error){
+                messageOne.textContent = data.error
+                return
+            }
+            messageOne.textContent = 'Location: ' + data.location
+            messageTwo.textContent = 'Forecast: ' + data.forecast
+        
+        })
+    })
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,26 @@
+const request = require('request')
+
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/b7a2e7e90b4af90a2620e1049048ef4a/' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude) + '?&units=si'
+    
+    request({ url, json: true }, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if(body.error) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, 
+                //{
+                'temperature: ' + body.currently.temperature + '. There is a ' + body.currently.precipProbability + '% changce of rain'
+                //temperature: response.body.currently.temperature,
+                //precipProbability: response.body.currently.precipProbability,
+                //timezone: response.body.timezone
+                //}
+            )
+        }
+
+    })
+}
+
+module.exports = forecast
